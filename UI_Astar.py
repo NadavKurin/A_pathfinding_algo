71aa01281de08971506a8356ea7f34baff037a33
Nadav <nadavku@post.bgu.ac.il>
2021-07-11 10:18:42 +0300

    Initial commit

---
+++ UI_Astar.py
@@ -0,0 +1,12 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH,WIDTH))
+pygame.display.set_caption("A* Path Finding Algorithm")
+
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,255,0)
+YELLOW = (255,255,0)
